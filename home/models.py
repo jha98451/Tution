c1dfb77f25a235cd67a9dd1fb85ffc3023fda1b3
Abhishek <jha98451@gmail.com>
2023-07-28 01:25:49 +0530

    Project Tution Serlizer

--- home/models.py
+++ home/models.py
@@ -1,19 +1,19 @@
-# from django.db import models
-#
-# # Create your models here.
-# class Login(models.Model):
-#     name = models.CharField(max_length=200)
-#     roll = models.CharField(max_length=10,default='S')
-#     userName = models.EmailField()
-#     userPassword = models.CharField(max_length=10)
-#     def __str__(self):
-#         fields = '__all__'
-#
-# class Profile(models.Model):
-#     name = models.ForeignKey(id,Login)
-#     profileImg = models.ImageField()
-#     specilization = models.CharField()
-#     course = models.CharField(default='IELTS')
-#     qulification = models.CharField()
-#     def __str__(self):
-#         fields = '__all__'
+from django.db import models
+
+# Create your models here.
+class Login(models.Model):
+    name = models.CharField(max_length=200)
+    roll = models.CharField(max_length=10,default='S')
+    userName = models.EmailField()
+    userPassword = models.CharField(max_length=10)
+    # def __str__(self):
+    #     fields = '__all__'
+
+class Profile(models.Model):
+    name = models.ForeignKey(Login,on_delete=models.CASCADE,related_name='Profile')
+    profileImg = models.ImageField()
+    specilization = models.CharField(max_length=200)
+    course = models.CharField(max_length=200,default='IELTS')
+    qulification = models.CharField(max_length=200)
+    # def __str__(self):
+    #     fields = '__all__'
